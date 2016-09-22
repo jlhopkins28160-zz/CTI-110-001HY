39501b5dcb7b2b2752e0925a6f1e464d0d3a5cbf
Johnny Hopkins <jhopkins1009@student.isothermal.edu>
2016-09-21 20:38:08 -0400

    Add files via upload
    My first Python program prompting user for input. The name of the user is displayed on the screen.

---
+++ hello.py
@@ -0,0 +1,9 @@
+#Johnny L. Hopkins
+#September 21, 2016
+#CTI 110-001HY: Web, Programming, and Database Foundations
+#Isothermal Community College, Spindale NC
+#Dr. Dana Anderson
+#hello.py: Prompts user for input and returns greeting
+
+name = input("What is your name? ")
+print("Hello, " + name)
